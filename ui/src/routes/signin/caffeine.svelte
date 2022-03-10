8260b39a691d898e267c600388426ac5e95b3b21
Ender <33337309+e3ndr@users.noreply.github.com>
2022-03-10 10:38:42 -0600

    Caffeinated: Fix Caffeine signins.

--- ui/src/routes/signin/caffeine.svelte
+++ ui/src/routes/signin/caffeine.svelte
@@ -2,6 +2,95 @@
     import { setPageProperties } from "../__layout.svelte";
     import { onMount } from "svelte";
 
+    let usernameInput = "";
+    let passwordInput = "";
+    let mfaInput = "";
+
+    let isLoading = false;
+    let isMfaPrompt = false;
+
+    let errorMessage = null;
+
+    function signinCaffeine() {
+        return new Promise((resolve, reject) => {
+            const loginPayload = {
+                account: {
+                    username: usernameInput,
+                    password: passwordInput
+                },
+                mfa: {
+                    otp: mfaInput
+                }
+            };
+
+            fetch("https://api.caffeine.tv/v1/account/signin", {
+                method: "POST",
+                body: JSON.stringify(loginPayload),
+                headers: new Headers({
+                    "Content-Type": "application/json"
+                })
+            })
+                .then((result) => result.json())
+                .then((response) => {
+                    if (response.hasOwnProperty("next")) {
+                        reject("CAFFEINE_MFA");
+                    } else if (response.errors) {
+                        reject(response.errors);
+                    } else {
+                        const refreshToken = response.refresh_token;
+
+                        fetch(`https://api.casterlabs.co/v2/natsukashii/create?platform=CAFFEINE&token=${refreshToken}`)
+                            .then((nResult) => nResult.json())
+                            .then((nResponse) => {
+                                if (nResponse.data) {
+                                    resolve(nResponse.data.token);
+                                } else {
+                                    reject(response.errors);
+                                }
+                            });
+                    }
+                })
+                .catch(reject);
+        });
+    }
+
+    async function submit(e) {
+        if (!e.code || e.code == "Enter") {
+            if (isLoading) {
+                return;
+            }
+
+            errorMessage = "";
+            isLoading = true;
+
+            try {
+                const token = await signinCaffeine();
+
+                Caffeinated.auth.caffeineSignin(token);
+            } catch (e) {
+                if (e == "CAFFEINE_MFA") {
+                    isMfaPrompt = true;
+                } else {
+                    if (e.otp) {
+                        errorMessage = "The Two Factor code is expired or incorrect.";
+                    } else if (e._error) {
+                        switch (e._error[0]) {
+                            case "The username or password provided is incorrect": {
+                                errorMessage = "The username or password provided is incorrect.";
+                                break;
+                            }
+                        }
+                    } else {
+                        errorMessage = JSON.stringify(e);
+                        console.error(e);
+                    }
+                }
+            } finally {
+                isLoading = false;
+            }
+        }
+    }
+
     setPageProperties({
         showSideBar: false,
         allowNavigateBackwards: false
@@ -19,170 +108,35 @@
     <h1 class="title is-4">Sign in with Caffeine</h1>
     <br />
     <div>
-        <input id="username-input" class="input" type="text" placeholder="Username" />
+        <input
+            id="username-input"
+            class="input"
+            type="text"
+            placeholder="Username"
+            bind:value={usernameInput}
+            on:keydown={(e) => e.code == "Enter" && document.querySelector("#password-input").focus()}
+        />
         <br />
-        <input id="password-input" class="input" type="password" placeholder="Password" />
+        <input id="password-input" class="input" type="password" placeholder="Password" bind:value={passwordInput} on:keydown={submit} />
         <br />
         <div class="hidden">
             <br />
-            <input id="mfa-input" class="input is-success" type="text" placeholder="Two Factor Code" />
+            <input id="mfa-input" class="input is-success" type="text" placeholder="Two Factor Code" bind:value={mfaInput} on:keydown={submit} />
             <br />
         </div>
         <br />
-        <span id="error-message" class="hidden is-6" />
-        <button id="signin-submit" class="button"> Sign In </button>
+        <span id="error-message" class="is-6 is-danger">
+            {#if errorMessage}
+                {errorMessage}
+                <br />
+            {/if}
+        </span>
+        <button id="signin-submit" class="button" class:is-loading={isLoading} on:click={submit}> Sign In </button>
         <br />
         <br />
         <!-- svelte-ignore a11y-missing-attribute -->
         <a onclick="history.back()" style="color: var(--theme);"> Want to go back? </a>
     </div>
-    <script>
-        {
-            const signinContainer = document.querySelector("#signin-container");
-            const usernameInput = signinContainer.querySelector("#username-input");
-            const passwordInput = signinContainer.querySelector("#password-input");
-            const mfaInput = signinContainer.querySelector("#mfa-input");
-            const errorMessage = signinContainer.querySelector("#error-message");
-            const signinSubmit = signinContainer.querySelector("#signin-submit");
-
-            /* ------------ */
-            /* Helpers      */
-            /* ------------ */
-
-            function signinCaffeine(username, password, mfa) {
-                return new Promise((resolve, reject) => {
-                    const loginPayload = {
-                        account: {
-                            username: username,
-                            password: password
-                        },
-                        mfa: {
-                            otp: mfa
-                        }
-                    };
-
-                    fetch("https://api.caffeine.tv/v1/account/signin", {
-                        method: "POST",
-                        body: JSON.stringify(loginPayload),
-                        headers: new Headers({
-                            "Content-Type": "application/json"
-                        })
-                    })
-                        .then((result) => result.json())
-                        .then((response) => {
-                            if (response.hasOwnProperty("next")) {
-                                reject("CAFFEINE_MFA");
-                            } else if (response.errors) {
-                                reject(response.errors);
-                            } else {
-                                const refreshToken = response.refresh_token;
-
-                                fetch(`https://api.casterlabs.co/v2/natsukashii/create?platform=CAFFEINE&token=${refreshToken}`)
-                                    .then((nResult) => nResult.json())
-                                    .then((nResponse) => {
-                                        if (nResponse.data) {
-                                            resolve(nResponse.data.token);
-                                        } else {
-                                            reject(response.errors);
-                                        }
-                                    });
-                            }
-                        })
-                        .catch(reject);
-                });
-            }
-
-            function clearFields() {
-                usernameInput.value = "";
-                passwordInput.value = "";
-                mfaInput.value = "";
-                mfaInput.parentElement.classList.add("hidden");
-                clearError();
-            }
-
-            function clearError() {
-                usernameInput.classList.remove("is-danger");
-                passwordInput.classList.remove("is-danger");
-                mfaInput.classList.remove("is-danger");
-                errorMessage.innerHTML = "";
-                errorMessage.classList.add("hidden");
-            }
-
-            function setErrorMessage(message) {
-                errorMessage.innerHTML = `${message} <br /><br />`;
-                errorMessage.classList.remove("hidden");
-            }
-
-            /* ------------ */
-            /* UX Features  */
-            /* ------------ */
-
-            // Make it so when you hit enter on the username input
-            // it'll automagically take you to the password field.
-            usernameInput.addEventListener("keyup", (e) => {
-                if (e.code == "Enter") {
-                    passwordInput.focus();
-                }
-            });
-
-            // Make the password and mfa fields trigger a signin attempt on enter.
-            {
-                passwordInput.addEventListener("keyup", (e) => {
-                    if (e.code == "Enter") {
-                        signinSubmit.click();
-                    }
-                });
-
-                mfaInput.addEventListener("keyup", (e) => {
-                    if (e.code == "Enter") {
-                        signinSubmit.click();
-                    }
-                });
-            }
-
-            /* ------------ */
-            /*    *Magic*   */
-            /* ------------ */
-
-            signinSubmit.addEventListener("click", async () => {
-                signinSubmit.classList.add("is-loading");
-                clearError();
-
-                try {
-                    const token = await signinCaffeine(usernameInput.value, passwordInput.value, mfaInput.value);
-
-                    Caffeinated.auth.caffeineSignin(token);
-                } catch (e) {
-                    if (e == "CAFFEINE_MFA") {
-                        clearError();
-                        mfaInput.parentElement.classList.remove("hidden");
-                        mfaInput.focus();
-                    } else {
-                        if (e.otp) {
-                            mfaInput.classList.add("is-danger");
-                            mfaInput.focus();
-                            setErrorMessage("The Two Factor code is expired or incorrect.");
-                        } else if (e._error) {
-                            switch (e._error[0]) {
-                                case "The username or password provided is incorrect": {
-                                    usernameInput.classList.add("is-danger");
-                                    passwordInput.classList.add("is-danger");
-                                    passwordInput.focus();
-                                    setErrorMessage("The username or password is incorrect.");
-                                    break;
-                                }
-                            }
-                        } else {
-                            setErrorMessage(JSON.stringify(e));
-                            console.error(e);
-                        }
-                    }
-                } finally {
-                    signinSubmit.classList.remove("is-loading");
-                }
-            });
-        }
-    </script>
 </div>
 
 <style>
