321dfd2169a4e2d327e27e698030364894ac1707
Ender <33337309+e3ndr@users.noreply.github.com>
2022-04-02 20:05:27 -0500

    Remove a nullable annotation.

--- app/PluginSDK/src/main/java/co/casterlabs/caffeinated/pluginsdk/kinoko/KinokoV2Listener.java
+++ app/PluginSDK/src/main/java/co/casterlabs/caffeinated/pluginsdk/kinoko/KinokoV2Listener.java
@@ -1,14 +1,12 @@
 package co.casterlabs.caffeinated.pluginsdk.kinoko;
 
-import org.jetbrains.annotations.Nullable;
-
 import co.casterlabs.rakurai.json.element.JsonObject;
 
 public interface KinokoV2Listener {
 
     public void onOpen(String id);
 
-    public void onMessage(JsonObject message, @Nullable String sender);
+    public void onMessage(JsonObject message, String sender);
 
     public void onJoin(String id);
 
