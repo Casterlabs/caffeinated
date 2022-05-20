8262b89c0b61b5b4d5ca80887c9ca2acac3a9ae1
Ender <33337309+e3ndr@users.noreply.github.com>
2022-05-20 12:38:08 -0500

    Caffeinated: Hush Mr. Compiley.

--- app/Util/src/main/java/co/casterlabs/caffeinated/util/MiscUtil.java
+++ app/Util/src/main/java/co/casterlabs/caffeinated/util/MiscUtil.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class MiscUtil {
 
+    @SafeVarargs
     public static <T> T random(T... items) {
         return items[ThreadLocalRandom.current().nextInt(items.length)];
     }
