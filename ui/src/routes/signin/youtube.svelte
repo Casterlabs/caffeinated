0c4b04202279290c5ca0716dc1ccb6d49cec006c
Ender <33337309+e3ndr@users.noreply.github.com>
2022-05-20 12:22:50 -0500

    Add assets for YouTube.

---
+++ ui/src/routes/signin/youtube.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { setPageProperties } from "../__layout.svelte";
+    import { onMount } from "svelte";
+
+    import OAuthSignin from "../../components/platforms/oauth-signin.svelte";
+
+    setPageProperties({
+        showSideBar: false,
+        allowNavigateBackwards: false
+    });
+
+    onMount(() => {
+        document.title = "";
+    });
+</script>
+
+<div id="signin-container">
+    <OAuthSignin platform="caffeinated_youtube" isKoi={true} />
+</div>
