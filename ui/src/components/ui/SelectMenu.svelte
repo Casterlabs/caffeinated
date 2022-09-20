4c00868779556ed153dfd3766c453a33744c5d72
Ender <33337309+e3ndr@users.noreply.github.com>
2022-09-20 02:04:27 -0500

    Caffeinated: Lock the height of the select menu.

--- ui/src/components/ui/SelectMenu.svelte
+++ ui/src/components/ui/SelectMenu.svelte
@@ -76,7 +76,7 @@
 				id={ID}
 				type="button"
 				role="listbox"
-				class="relative w-full cursor-pointer rounded-md border border-mauve-7 bg-mauve-1 py-2 pl-3 pr-10 text-left shadow-sm focus:border-crimson-7 focus:outline-none focus:ring-1 focus:ring-crimson-7 text-sm"
+				class="relative w-full h-[2.375rem] cursor-pointer rounded-md border border-mauve-7 bg-mauve-1 py-2 pl-3 pr-10 text-left shadow-sm focus:border-crimson-7 focus:outline-none focus:ring-1 focus:ring-crimson-7 text-sm"
 				aria-haspopup="listbox"
 				aria-expanded={open}
 				on:click={() => (open = !open)}
