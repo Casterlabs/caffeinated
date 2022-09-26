f7312cf46efe26a829f52ea4b240b4f63b3a2fd2
Ender <33337309+e3ndr@users.noreply.github.com>
2022-09-26 09:13:53 -0500

    Caffeinated: Create a modal helper.

---
+++ ui/src/components/layout/Modal.svelte
@@ -0,0 +1,38 @@
+<script>
+	import LocalizedText from '../LocalizedText.svelte';
+
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function close() {
+		dispatch('close');
+	}
+</script>
+
+<div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+	<div class="fixed inset-0 bg-mauve-6 opacity-75 transition-opacity" />
+
+	<div class="fixed inset-0 z-10 overflow-y-auto">
+		<div class="flex min-h-full items-center justify-center text-center">
+			<div
+				class="relative transform overflow-hidden rounded-lg bg-mauve-1 text-left shadow-xl transition-all px-6 pb-5 pt-3.5 w-full max-w-3xl"
+			>
+				<h3 class="text-lg font-medium text-mauve-12 mb-4" id="modal-title">
+					<slot name="title" />
+				</h3>
+
+				<button class="absolute top-4 right-3.5 text-mauve-12" on:click={close}>
+					<span class="sr-only">
+						<LocalizedText key="sr.modal.close" />
+					</span>
+					<icon data-icon="x-mark" />
+				</button>
+
+				<div class="text-mauve-10">
+					<slot />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
