2e42a122a588703ce1dda8c6a42f1b9486fe7ca6
Ender <33337309+e3ndr@users.noreply.github.com>
2022-09-26 03:23:15 -0500

    Caffeinated: Add a pagetitle helper.

---
+++ ui/src/components/PageTitle.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { t } from '$lib/translate.mjs';
+	import { language } from '$lib/app.mjs';
+	import { onDestroy } from 'svelte';
+
+	export let title;
+
+	onDestroy(() => {
+		document.title = 'Casterlabs-Caffeinated';
+	});
+</script>
+
+<svelte:head>
+	<title>
+		Casterlabs-Caffeinated - {t($language, title)}
+	</title>
+</svelte:head>
