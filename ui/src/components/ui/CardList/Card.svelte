3e3f25d4386cd674f56a9a7531a8582c518a2048
Ender <33337309+e3ndr@users.noreply.github.com>
2022-09-26 10:20:03 -0500

    Caffeinated: Move the widgets page over to the new CardList.
    Also renamed a variable.

--- ui/src/components/ui/CardList/Card.svelte
+++ ui/src/components/ui/CardList/Card.svelte
@@ -2,16 +2,28 @@
 	import LocalizedText from '../../LocalizedText.svelte';
 
 	import { createEventDispatcher } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	const dispatch = createEventDispatcher();
 
 	export let icon = null;
 	export let text = null;
+
+	export let href = null;
+
+	function onClick() {
+		if (href) {
+			goto(href);
+		} else {
+			dispatch('click');
+		}
+	}
 </script>
 
 <button
+	role={href ? 'link' : 'button'}
 	class="relative flex items-center space-x-3 rounded-lg border border-mauve-6 bg-mauve-2 p-5 shadow-sm focus:border-crimson-7 focus:outline-none focus:ring-1 focus:ring-crimson-7"
-	on:click={() => dispatch('click')}
+	on:click={onClick}
 >
 	<div class="flex-shrink-0 text-mauve-12">
 		<icon data-icon={icon} />
--- ui/src/routes/widgets/index.svelte
+++ ui/src/routes/widgets/index.svelte
@@ -2,16 +2,18 @@
 	import Modal from '../../components/layout/Modal.svelte';
 	import PageTitle from '../../components/PageTitle.svelte';
 	import LocalizedText from '../../components/LocalizedText.svelte';
+	import CardList from '../../components/ui/CardList/index.svelte';
+	import Card from '../../components/ui/cardlist/Card.svelte';
 
 	import { onMount } from 'svelte';
 	import { t } from '$lib/translate.mjs';
 
-	let docks = [];
+	let widgets = [];
 	let showingTutorialModal = false;
 	let showingCreationModal = false;
 
 	onMount(async () => {
-		docks = (await Caffeinated.plugins.widgets).filter((w) => w.details.type == 'WIDGET');
+		widgets = (await Caffeinated.plugins.widgets).filter((w) => w.details.type == 'WIDGET');
 	});
 </script>
 
@@ -46,33 +48,27 @@
 	</Modal>
 {/if}
 
-<div class="mt-8 grid gap-4 grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5">
-	{#each docks as dock}
-		<a
-			class="relative flex items-center space-x-3 rounded-lg border border-mauve-6 bg-mauve-2 p-5 shadow-sm focus:border-crimson-7 focus:outline-none focus:ring-1 focus:ring-crimson-7"
-			href="/widgets/edit?id={dock.id}"
-		>
-			<div class="flex-shrink-0 text-mauve-12">
-				<icon data-icon={dock.details.icon} />
-			</div>
-			<div class="min-w-0 flex-1">
-				<p class="text-sm font-medium text-mauve-12 text-left">
-					{dock.details.friendlyName}
-				</p>
-			</div>
-		</a>
-	{/each}
+<div class="mt-8">
+	<CardList>
+		{#each widgets as widget}
+			<Card
+				icon={widget.details.icon}
+				text={widget.details.friendlyName}
+				href="/widgets/edit?id={widget.id}"
+			/>
+		{/each}
 
-	<button
-		class="relative flex items-center justify-center rounded-lg border border-mauve-4 bg-mauve-1 p-5 focus:border-crimson-7 focus:outline-none focus:ring-1 focus:ring-crimson-7"
-		title={t('sr.page.widgets.create')}
-		on:click={() => (showingCreationModal = true)}
-	>
-		<div class="text-mauve-12">
-			<span class="sr-only">
-				<LocalizedText key="sr.page.widgets.create" />
-			</span>
-			<icon data-icon="plus" />
-		</div>
-	</button>
+		<button
+			class="relative flex items-center justify-center rounded-lg border border-mauve-4 bg-mauve-1 p-5 focus:border-crimson-7 focus:outline-none focus:ring-1 focus:ring-crimson-7"
+			title={t('sr.page.widgets.create')}
+			on:click={() => (showingCreationModal = true)}
+		>
+			<div class="text-mauve-12">
+				<span class="sr-only">
+					<LocalizedText key="sr.page.widgets.create" />
+				</span>
+				<icon data-icon="plus" />
+			</div>
+		</button>
+	</CardList>
 </div>
