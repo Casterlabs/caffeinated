687930a00eaf55f11b8a2f3ec4efd82498b869e4
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-05 13:22:35 -0500

    Caffeinated: Restructure the project, big commit imminent.

--- ui/src/lib/app.mjs
+++
@@ -1,7 +0,0 @@
-import { FALLBACK_LANGUAGE } from '$lib/translate.mjs';
-import {get, writable } from 'svelte/store';
-
-export const language = writable(FALLBACK_LANGUAGE);
-export const icon = writable('casterlabs');
-export const iconColor = writable('white'); // "black", "white"
-export const hasCasterlabsPlus = writable(false);
