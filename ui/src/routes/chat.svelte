34401847f4ced8c6ba58f758ac118c8c632e3679
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-05 14:40:33 -0500

    Caffeinated: Populate the viewers list on load.

--- ui/src/routes/chat.svelte
+++ ui/src/routes/chat.svelte
@@ -30,6 +30,17 @@
 		const eventListener = Bridge.on('koi:event', chatViewer.processEvent);
 		Caffeinated.koi.eventHistory.then((h) => h.forEach(chatViewer.processEvent));
 
+		Caffeinated.koi.viewers.then((all) => {
+			for (const [platform, viewers] of Object.entries(all)) {
+				chatViewer.viewersList.onViewersList({
+					streamer: {
+						platform
+					},
+					viewers
+				});
+			}
+		});
+
 		return () => {
 			Bridge.off('koi:event', eventListener);
 		};
