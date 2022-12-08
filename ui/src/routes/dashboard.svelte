fbdb88f6b7a4c14a2cc81e2dcc87a9600e56496e
Ender <33337309+e3ndr@users.noreply.github.com>
2022-12-08 00:27:08 -0600

    Caffeinated: Work on more layout for the dashboard.

--- ui/src/routes/dashboard.svelte
+++ ui/src/routes/dashboard.svelte
@@ -1,18 +1,34 @@
 <script>
 	import PageTitle from '../components/PageTitle.svelte';
 	import CustomResizableGrid from '$lib/layout/CustomResizableGrid.svelte';
+	import NumberInput from '$lib/ui/NumberInput.svelte';
 
-	let layout;
+	import { onMount } from 'svelte';
+	import createConsole from '$lib/console-helper.mjs';
 
-	function onLayoutUpdate({ detail: layout }) {
-		console.debug('Layout mount:', layout);
+	const console = createConsole('Dashboard');
+
+	let layoutElement;
+	let layout; // TODO load and save this to caffeinated.
+
+	$: width = layout?.v?.length + 1 || 0;
+	$: height = layout?.h?.length + 1 || 0;
+
+	function onLayoutUpdate({ detail: _layout }) {
+		layout = _layout;
+		console.debug('Layout update:', layout);
+		// TODO save
 	}
+
+	onMount(() => {
+		// TODO load
+	});
 </script>
 
 <PageTitle title="page.dashboard" />
 
 <div class="fixed inset-0 left-48">
-	<CustomResizableGrid bind:this={layout} on:update={onLayoutUpdate}>
+	<CustomResizableGrid bind:this={layoutElement} on:update={onLayoutUpdate}>
 		<div slot="0,0">0,0</div>
 		<div slot="1,0">1,0</div>
 		<div slot="2,0">2,0</div>
@@ -53,7 +69,39 @@
 </div>
 
 <div
-	class="fixed top-0 right-4 h-7 -translate-y-6 hover:translate-y-0 px-2 py-0.5 transition-all rounded-b-md ring-1 ring-base-7 bg-base-5 text-base-12 opacity-90"
+	class="fixed top-0 right-4 h-9 -translate-y-8 hover:translate-y-0 px-2 py-0.5 transition-all rounded-b-md ring-1 ring-base-7 bg-base-5 text-base-12 opacity-90"
 >
-	TODO
+	<div class="inline-block w-12">
+		<NumberInput
+			value={width}
+			min="1"
+			max="6"
+			on:value={({ detail: newWidth }) => {
+				if (newWidth < width) {
+					layout.h.pop();
+					layoutElement.updateLayout(layout);
+				} else {
+					// TODO
+				}
+				console.debug('New width:', newWidth);
+			}}
+		/>
+	</div>
+	x
+	<div class="inline-block w-12">
+		<NumberInput
+			value={height}
+			min="1"
+			max="6"
+			on:value={({ detail: newHeight }) => {
+				if (newHeight < height) {
+					layout.v.pop();
+					layoutElement.updateLayout(layout);
+				} else {
+					// TODO
+				}
+				console.debug('New height:', newHeight);
+			}}
+		/>
+	</div>
 </div>
