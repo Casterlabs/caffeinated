f02c4a6b289182303064da60f7f2571b8a593a40
Ender <33337309+e3ndr@users.noreply.github.com>
2022-12-08 00:04:01 -0600

    Caffeinated: Actually, the grid size should be customizable.

---
+++ ui/src/lib/layout/CustomResizableGrid.svelte
@@ -0,0 +1,168 @@
+<script>
+	import { onMount, createEventDispatcher, tick } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let width = 3;
+	let height = 3;
+	let vlayout = [0.25, 0.33]; // last item is implied.
+	let hlayout = [0.33, 0.25]; // last item is implied.
+
+	let slotContents = {};
+	let slotElements = {};
+
+	function onLayoutUpdated() {
+		// Loop bind the slotContents to slotElements.
+		dispatch('mount');
+	}
+
+	onMount(onLayoutUpdated);
+
+	export function updateLayout(newLayout, newWidth, newHeight) {
+		width = newWidth;
+		height = newHeight;
+		vlayout = newLayout.v;
+		hlayout = newLayout.h;
+		console.debug(newLayout);
+		tick().then(onLayoutUpdated);
+	}
+</script>
+
+<div class="hlayout">
+	{#each Array(height) as _, hindex}
+		{@const isHLast = hindex == height - 1}
+		{@const hSize = isHLast ? 1 - hlayout.reduce((p, i) => p + i, 0) : hlayout[hindex]}
+
+		<div class="hslot-area" style="--size: {hSize};">
+			<div class="vlayout">
+				{#each Array(width) as _, vindex}
+					{@const isVLast = vindex == width - 1}
+					{@const vSize = isVLast ? 1 - vlayout.reduce((p, i) => p + i, 0) : vlayout[vindex]}
+					{@const location = `${hindex},${vindex}`}
+
+					<div
+						class="vslot-area"
+						style="--size: {vSize};"
+						bind:this={slotElements[location]}
+						data-position={location}
+					/>
+
+					{#if !isVLast}
+						<!-- Add the ver sizer everywhere except the last item. -->
+						<div class="vsizer-bar oversize">
+							<div class="inner" />
+						</div>
+					{/if}
+				{/each}
+			</div>
+		</div>
+
+		{#if !isHLast}
+			<!-- Add the hoz sizer everywhere except the last item. -->
+			<div class="hsizer-bar">
+				<div class="inner" />
+			</div>
+		{/if}
+	{/each}
+</div>
+
+<div style="display: none;" aria-hidden="true">
+	<!-- We need 10 of these -->
+	<span bind:this={slotContents['0,0']}><slot name="0,0" /></span>
+	<span bind:this={slotContents['0,1']}><slot name="0,1" /></span>
+	<span bind:this={slotContents['0,2']}><slot name="0,2" /></span>
+	<span bind:this={slotContents['0,3']}><slot name="0,3" /></span>
+	<span bind:this={slotContents['0,4']}><slot name="0,4" /></span>
+	<span bind:this={slotContents['0,5']}><slot name="0,5" /></span>
+	<span bind:this={slotContents['1,0']}><slot name="1,0" /></span>
+	<span bind:this={slotContents['1,1']}><slot name="1,1" /></span>
+	<span bind:this={slotContents['1,2']}><slot name="1,2" /></span>
+	<span bind:this={slotContents['1,3']}><slot name="1,3" /></span>
+	<span bind:this={slotContents['1,4']}><slot name="1,4" /></span>
+	<span bind:this={slotContents['1,5']}><slot name="1,5" /></span>
+	<span bind:this={slotContents['2,0']}><slot name="2,0" /></span>
+	<span bind:this={slotContents['2,1']}><slot name="2,1" /></span>
+	<span bind:this={slotContents['2,2']}><slot name="2,2" /></span>
+	<span bind:this={slotContents['2,3']}><slot name="2,3" /></span>
+	<span bind:this={slotContents['2,4']}><slot name="2,4" /></span>
+	<span bind:this={slotContents['2,5']}><slot name="2,5" /></span>
+	<span bind:this={slotContents['3,0']}><slot name="3,0" /></span>
+	<span bind:this={slotContents['3,1']}><slot name="3,1" /></span>
+	<span bind:this={slotContents['3,2']}><slot name="3,2" /></span>
+	<span bind:this={slotContents['3,3']}><slot name="3,3" /></span>
+	<span bind:this={slotContents['3,4']}><slot name="3,4" /></span>
+	<span bind:this={slotContents['3,5']}><slot name="3,5" /></span>
+	<span bind:this={slotContents['4,0']}><slot name="4,0" /></span>
+	<span bind:this={slotContents['4,1']}><slot name="4,1" /></span>
+	<span bind:this={slotContents['4,2']}><slot name="4,2" /></span>
+	<span bind:this={slotContents['4,3']}><slot name="4,3" /></span>
+	<span bind:this={slotContents['4,4']}><slot name="4,4" /></span>
+	<span bind:this={slotContents['4,5']}><slot name="4,5" /></span>
+	<span bind:this={slotContents['5,0']}><slot name="5,0" /></span>
+	<span bind:this={slotContents['5,1']}><slot name="5,1" /></span>
+	<span bind:this={slotContents['5,2']}><slot name="5,2" /></span>
+	<span bind:this={slotContents['5,3']}><slot name="5,3" /></span>
+	<span bind:this={slotContents['5,4']}><slot name="5,4" /></span>
+	<span bind:this={slotContents['5,5']}><slot name="5,5" /></span>
+</div>
+
+<style>
+	.hlayout {
+		--sizer-bar-size: 7px;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 100%;
+	}
+
+	.hslot-area {
+		width: 100%;
+		height: calc(var(--size) * 100% - var(--sizer-bar-size) / 2);
+	}
+
+	.hsizer-bar {
+		width: 100%;
+		height: var(--sizer-bar-size);
+		cursor: row-resize;
+	}
+
+	.hsizer-bar .inner {
+		width: 100%;
+		margin-top: 3px;
+		background-color: var(--base11);
+		height: 1px;
+	}
+
+	.vlayout {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		height: 100%;
+	}
+
+	.vslot-area {
+		overflow: hidden;
+		height: 100%;
+		width: calc(var(--size) * 100% - var(--sizer-bar-size));
+	}
+
+	.vsizer-bar {
+		height: 100%;
+		width: var(--sizer-bar-size);
+		cursor: col-resize;
+	}
+
+	.vsizer-bar .inner {
+		height: 100%;
+		margin-left: 3px;
+		background-color: var(--base11);
+		width: 1px;
+	}
+
+	.vsizer-bar.oversize .inner {
+		height: calc(100% + var(--sizer-bar-size));
+		margin-left: 3px;
+		background-color: var(--base11);
+		width: 1px;
+	}
+</style>
--- ui/src/routes/dashboard.svelte
+++ ui/src/routes/dashboard.svelte
@@ -1,19 +1,10 @@
 <script>
 	import PageTitle from '../components/PageTitle.svelte';
-	import ResizableGrid from '$lib/layout/ResizableGrid.svelte';
-	import ResizableHColumns from '$lib/layout/ResizableHColumns.svelte';
-	import ResizableVColumns from '$lib/layout/ResizableVColumns.svelte';
+	import CustomResizableGrid from '$lib/layout/CustomResizableGrid.svelte';
 
-	let layout = 'GRID'; // GRID, HCOLUMNS, VCOLUMNS
-	$: layoutClass = {
-		GRID: ResizableGrid,
-		HCOLUMNS: ResizableHColumns,
-		VCOLUMNS: ResizableVColumns
-	}[layout];
+	let layout;
 
-	let updateLayout = () => {};
-	function onLayoutMount(_updateLayout) {
-		updateLayout = _updateLayout;
+	function onLayoutMount() {
 		console.debug('Layout mount');
 	}
 </script>
@@ -21,27 +12,48 @@
 <PageTitle title="page.dashboard" />
 
 <div class="fixed inset-0 left-48">
-	<svelte:component this={layoutClass} {onLayoutMount}>
-		<div slot="0">0</div>
-		<div slot="1">1</div>
-		<div slot="2">2</div>
-		<div slot="3">3</div>
-	</svelte:component>
+	<CustomResizableGrid bind:this={layout} on:mount={onLayoutMount}>
+		<div slot="0,0">0,0</div>
+		<div slot="0,1">0,1</div>
+		<div slot="0,2">0,2</div>
+		<div slot="0,3">0,3</div>
+		<div slot="0,4">0,4</div>
+		<div slot="0,5">0,5</div>
+		<div slot="1,0">1,0</div>
+		<div slot="1,1">1,1</div>
+		<div slot="1,2">1,2</div>
+		<div slot="1,3">1,3</div>
+		<div slot="1,4">1,4</div>
+		<div slot="1,5">1,5</div>
+		<div slot="2,0">2,0</div>
+		<div slot="2,1">2,1</div>
+		<div slot="2,2">2,2</div>
+		<div slot="2,3">2,3</div>
+		<div slot="2,4">2,4</div>
+		<div slot="2,5">2,5</div>
+		<div slot="3,0">3,0</div>
+		<div slot="3,1">3,1</div>
+		<div slot="3,2">3,2</div>
+		<div slot="3,3">3,3</div>
+		<div slot="3,4">3,4</div>
+		<div slot="3,5">3,5</div>
+		<div slot="4,0">4,0</div>
+		<div slot="4,1">4,1</div>
+		<div slot="4,2">4,2</div>
+		<div slot="4,3">4,3</div>
+		<div slot="4,4">4,4</div>
+		<div slot="4,5">4,5</div>
+		<div slot="5,0">5,0</div>
+		<div slot="5,1">5,1</div>
+		<div slot="5,2">5,2</div>
+		<div slot="5,3">5,3</div>
+		<div slot="5,4">5,4</div>
+		<div slot="5,5">5,5</div>
+	</CustomResizableGrid>
 </div>
 
 <div
 	class="fixed top-0 right-4 h-7 -translate-y-6 hover:translate-y-0 px-2 py-0.5 transition-all rounded-b-md ring-1 ring-base-7 bg-base-5 text-base-12 opacity-90"
 >
-	<button class="scale-[1.1]" on:click={() => (layout = 'HCOLUMNS')}>
-		<icon data-icon="icon/view-columns" />
-	</button>
-	<button
-		class="rotate-90 scale-[.90] -translate-x-[0.03rem]"
-		on:click={() => (layout = 'VCOLUMNS')}
-	>
-		<icon data-icon="icon/view-columns" />
-	</button>
-	<button on:click={() => (layout = 'GRID')}>
-		<icon data-icon="icon/squares-2x2" />
-	</button>
+	TODO
 </div>
