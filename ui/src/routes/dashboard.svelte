7773c0b78d47b30fdbf31ac82d3bef74f2d203fc
Ender <33337309+e3ndr@users.noreply.github.com>
2022-12-08 00:49:55 -0600

    Common: Cleanup how the slots are passed to CustomResizableGrid.

--- ui/src/routes/dashboard.svelte
+++ ui/src/routes/dashboard.svelte
@@ -27,45 +27,19 @@
 
 <PageTitle title="page.dashboard" />
 
+<div class="hidden">
+	{#each Array(width) as _, hindex}
+		{#each Array(height) as _, vindex}
+			{@const location = `${vindex},${hindex}`}
+			<div bind:this={layoutElement.slotContents[location]}>
+				{location}
+			</div>
+		{/each}
+	{/each}
+</div>
+
 <div class="fixed inset-0 left-48">
-	<CustomResizableGrid bind:this={layoutElement} on:update={onLayoutUpdate}>
-		<div slot="0,0">0,0</div>
-		<div slot="1,0">1,0</div>
-		<div slot="2,0">2,0</div>
-		<div slot="3,0">3,0</div>
-		<div slot="4,0">4,0</div>
-		<div slot="5,0">5,0</div>
-		<div slot="0,1">0,1</div>
-		<div slot="1,1">1,1</div>
-		<div slot="2,1">2,1</div>
-		<div slot="3,1">3,1</div>
-		<div slot="4,1">4,1</div>
-		<div slot="5,1">5,1</div>
-		<div slot="0,2">0,2</div>
-		<div slot="1,2">1,2</div>
-		<div slot="2,2">2,2</div>
-		<div slot="3,2">3,2</div>
-		<div slot="4,2">4,2</div>
-		<div slot="5,2">5,2</div>
-		<div slot="0,3">0,3</div>
-		<div slot="1,3">1,3</div>
-		<div slot="2,3">2,3</div>
-		<div slot="3,3">3,3</div>
-		<div slot="4,3">4,3</div>
-		<div slot="5,3">5,3</div>
-		<div slot="0,4">0,4</div>
-		<div slot="1,4">1,4</div>
-		<div slot="2,4">2,4</div>
-		<div slot="3,4">3,4</div>
-		<div slot="4,4">4,4</div>
-		<div slot="5,4">5,4</div>
-		<div slot="0,5">0,5</div>
-		<div slot="1,5">1,5</div>
-		<div slot="2,5">2,5</div>
-		<div slot="3,5">3,5</div>
-		<div slot="4,5">4,5</div>
-		<div slot="5,5">5,5</div>
-	</CustomResizableGrid>
+	<CustomResizableGrid bind:this={layoutElement} on:update={onLayoutUpdate} />
 </div>
 
 <div
