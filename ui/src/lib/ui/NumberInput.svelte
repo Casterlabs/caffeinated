ab8c4f5436d0e85bfadd6bc2849db284c6a3f9f3
Ender <33337309+e3ndr@users.noreply.github.com>
2022-12-23 12:42:36 -0600

    Common: NumberInput.disabled

--- ui/src/lib/ui/NumberInput.svelte
+++ ui/src/lib/ui/NumberInput.svelte
@@ -9,6 +9,7 @@
 	export let value = 0;
 	export let min = Number.MIN_SAFE_INTEGER;
 	export let max = Number.MAX_SAFE_INTEGER;
+	export let disabled = false;
 
 	function db() {
 		if (value < min) {
@@ -25,11 +26,14 @@
 
 <input
 	class="px-2 py-1 w-full text-base-12 rounded-md border transition hover:border-base-8 border-base-7 bg-base-1 shadow-sm focus:border-primary-7 focus:outline-none focus:ring-1 focus:ring-primary-7 text-sm"
+	class:opacity-60={disabled}
 	type="number"
 	{min}
 	{max}
+	{disabled}
 	bind:value
 	on:mousewheel={(e) => {
+		if (disabled) return;
 		value -= e.deltaY / 100;
 		db();
 	}}
