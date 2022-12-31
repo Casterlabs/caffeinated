fef2c865eaa4a6d3470aa6cc28d61fb51b3f2490
Ender <33337309+e3ndr@users.noreply.github.com>
2022-12-31 13:03:42 -0600

    Common: Make modal sizing smarter.

--- ui/src/lib/ui/Modal.svelte
+++ ui/src/lib/ui/Modal.svelte
@@ -24,7 +24,7 @@
 	<div class="fixed inset-0 z-10 overflow-y-auto">
 		<div class="flex min-h-full items-center justify-center text-center">
 			<div
-				class="relative transform overflow-hidden rounded-lg bg-base-1 text-left shadow-xl transition-all px-6 pb-5 pt-3.5 w-full max-w-3xl"
+				class="relative transform overflow-hidden rounded-lg bg-base-1 text-left shadow-xl transition-all px-6 pb-5 pt-3.5 w-fit max-w-3xl"
 			>
 				<h3 class="text-lg font-medium text-base-12 mb-4" id="modal-title">
 					<slot name="title" />
