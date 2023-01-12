7681e9ce1d53c5989e43ae163f40510cc37aab88
Ender <33337309+e3ndr@users.noreply.github.com>
2023-01-12 04:29:40 -0600

    Caffeinated: Use a better GC to reduce the memory footprint :^)

--- build.sh
+++ build.sh
@@ -30,6 +30,7 @@ rm -rf dist/*
 mkdir -p dist
 
 KAIMEN_VERSION="7257f2e12c"
+VM_OPTIONS="-vm XX:+UnlockExperimentalVMOptions -vm XX:+UseShenandoahGC -vm XX:ShenandoahUncommitDelay=500 -vm XX:ShenandoahGuaranteedGCInterval=2000 -vm XX:ShenandoahGCHeuristics=compact"
 
 if [[ $@ != *"nodist"* ]]; then
     cp app/Bootstrap/target/classes/commit.txt dist
@@ -38,13 +39,13 @@ if [[ $@ != *"nodist"* ]]; then
     echo "Completing packaging of application."
     echo ""
 
-    java -jar ProjectBuilder.jar -os MACOSX -arch AMD64 -wi WEBKIT -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar -i icon.icns -id co.casterlabs.caffeinated
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os MACOSX -arch AMD64 -wi WEBKIT -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar -i icon.icns -id co.casterlabs.caffeinated
     echo ""
 
-    java -jar ProjectBuilder.jar -os WINDOWS -arch AMD64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -res app/WMC-JsonConsoleWrapper.exe -cp app/Bootstrap/target/Caffeinated.jar -i icon.ico
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os WINDOWS -arch AMD64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -res app/WMC-JsonConsoleWrapper.exe -cp app/Bootstrap/target/Caffeinated.jar -i icon.ico
     echo ""
 
-    java -jar ProjectBuilder.jar -os LINUX -arch AMD64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os LINUX -arch AMD64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar
     echo ""
 
 fi
