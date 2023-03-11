cbedbef7bf87b3124ec056a5202e9033bac00c57
Ender <33337309+e3ndr@users.noreply.github.com>
2023-03-10 22:47:50 -0600

    Caffeinated: Hopefully fix the chat widget's disappearing behavior 🤞

--- widgets/src/lib/chat/CardContainer.svelte
+++ widgets/src/lib/chat/CardContainer.svelte
@@ -1,11 +1,46 @@
 <svelte:options accessors />
 
 <script>
+	import { fade } from 'svelte/transition';
+
 	export let settings; // svelte store
 	export let offsetX = 0;
+
+	let visible = false;
+
+	let disappearTask = -1;
+	function checkDisappear() {
+		clearTimeout(disappearTask);
+
+		if (!$settings['message_style.disappearing']) {
+			visible = true;
+			return;
+		}
+
+		const secondsSinceAdded = Date.now() - new Date(event.timestamp);
+		const disappearAfter = $settings['message_style.disappear_after'] * 1000;
+		const disappearIn = disappearAfter - secondsSinceAdded;
+
+		if (disappearIn > 0) {
+			visible = true;
+			disappearTask = setTimeout(() => {
+				visible = false;
+			}, disappearIn);
+		} else {
+			visible = false;
+		}
+	}
+	onMount(() => settings.subscribe(checkDisappear));
 </script>
 
-<div id="slot" style:--x-offset={offsetX} />
+{#if visible}
+	<div
+		id="slot"
+		style:--x-offset={offsetX}
+		in:fade={{ duration: 200 }}
+		out:fade={{ duration: 750 }}
+	/>
+{/if}
 
 <style>
 	div {
--- widgets/src/lib/chat/RichMessage.svelte
+++ widgets/src/lib/chat/RichMessage.svelte
@@ -2,15 +2,15 @@
 
 <script>
 	import { onMount } from 'svelte';
-	import { fade } from 'svelte/transition';
 
 	let container;
 
 	export let event;
 	export let settings; // svelte store
 
+	$: badges = $settings['message_style.badges'];
+
 	export let upvotes = event.upvotes;
-	export let visible = false;
 
 	function updateUpvotes() {
 		if (!container) return;
@@ -20,69 +20,36 @@
 	}
 	$: upvotes, updateUpvotes();
 	onMount(updateUpvotes);
-
-	let disappearTask = -1;
-	function checkDisappear() {
-		clearTimeout(disappearTask);
-
-		if (!$settings['message_style.disappearing']) {
-			visible = true;
-			return;
-		}
-
-		const secondsSinceAdded = Date.now() - new Date(event.timestamp);
-		const disappearAfter = $settings['message_style.disappear_after'] * 1000;
-		const disappearIn = disappearAfter - secondsSinceAdded;
-
-		if (disappearIn > 0) {
-			visible = true;
-			disappearTask = setTimeout(() => {
-				visible = false;
-			}, disappearIn);
-		} else {
-			visible = false;
-		}
-	}
-	onMount(() => settings.subscribe(checkDisappear));
 </script>
 
-{#if visible}
-	{@const badges = $settings['message_style.badges']}
+<span class="inline-block" bind:this={container}>
+	{#if badges == 'Before Username'}
+		<span class="richmessage-badges space-x-1" aria-hidden="true">
+			{#each event.sender.badges as badge}
+				<img class="inline-block h-4 -translate-y-0.5" alt="" src={badge} />
+			{/each}
+		</span>
+	{/if}
+
+	<b style:color={event.sender.color || 'red'}>{event.sender.displayname}</b><span>:</span>
+
+	{#if badges == 'After Username'}
+		<span class="richmessage-badges space-x-1" aria-hidden="true">
+			{#each event.sender.badges as badge}
+				<img class="inline-block h-4 -translate-y-0.5" alt="" src={badge} />
+			{/each}
+		</span>
+	{/if}
 
 	<span
-		class="inline-block"
-		bind:this={container}
-		in:fade={{ duration: 200 }}
-		out:fade={{ duration: 750 }}
+		class:upvote-1={upvotes > 0}
+		class:upvote-2={upvotes > 10}
+		class:upvote-3={upvotes > 100}
+		class:upvote-4={upvotes > 1000}
 	>
-		{#if badges == 'Before Username'}
-			<span class="richmessage-badges space-x-1" aria-hidden="true">
-				{#each event.sender.badges as badge}
-					<img class="inline-block h-4 -translate-y-0.5" alt="" src={badge} />
-				{/each}
-			</span>
-		{/if}
-
-		<b style:color={event.sender.color || 'red'}>{event.sender.displayname}</b><span>:</span>
-
-		{#if badges == 'After Username'}
-			<span class="richmessage-badges space-x-1" aria-hidden="true">
-				{#each event.sender.badges as badge}
-					<img class="inline-block h-4 -translate-y-0.5" alt="" src={badge} />
-				{/each}
-			</span>
-		{/if}
-
-		<span
-			class:upvote-1={upvotes > 0}
-			class:upvote-2={upvotes > 10}
-			class:upvote-3={upvotes > 100}
-			class:upvote-4={upvotes > 1000}
-		>
-			{@html event.html}
-		</span>
+		{@html event.html}
 	</span>
-{/if}
+</span>
 
 <style>
 	/* Upvotes */
