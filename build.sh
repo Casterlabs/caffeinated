5f968240e187801701ba328538622df4f4b16038
Ender <33337309+e3ndr@users.noreply.github.com>
2023-03-12 02:09:41 -0600

    Caffeinated: Ah, wrong directory.

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ if [[ $@ != *"nodist"* ]]; then
     echo ""
 
     java -jar ProjectBuilder.jar $VM_OPTIONS -os MACOSX -arch AMD64 -wi WEBKIT -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar -i icon.icns -id co.casterlabs.caffeinated
-    cd caffeinated/dist/macOS-amd64
+    cd dist/macOS-amd64
     zip -r ../artifacts/macOS-amd64.zip *
     rm -rf ./jre
     zip -r ../artifacts/macOS-amd64-nojre.zip *
@@ -66,7 +66,7 @@ if [[ $@ != *"nodist"* ]]; then
     echo ""
 
     java -jar ProjectBuilder.jar $VM_OPTIONS -os WINDOWS -arch AMD64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -res app/WMC-JsonConsoleWrapper.exe -cp app/Bootstrap/target/Caffeinated.jar -i icon.ico
-    cd caffeinated/dist/Windows-amd64
+    cd dist/Windows-amd64
     zip -r ../artifacts/Windows-amd64.zip *
     rm -rf ./jre
     zip -r ../artifacts/Windows-amd64-nojre.zip *
@@ -74,7 +74,7 @@ if [[ $@ != *"nodist"* ]]; then
     echo ""
 
     java -jar ProjectBuilder.jar $VM_OPTIONS -os LINUX -arch AMD64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar
-    cd caffeinated/dist/Linux-amd64
+    cd dist/Linux-amd64
     zip -r ../artifacts/Linux-amd64.zip *
     rm -rf ./jre
     zip -r ../artifacts/Linux-amd64-nojre.zip *
