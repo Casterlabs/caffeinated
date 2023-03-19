0e54cb826a8aaaa0c2d279c561a452ddadaeeb7a
Ender <33337309+e3ndr@users.noreply.github.com>
2023-03-19 18:28:03 -0500

    Caffeinated: Assign a "random" port when in developer mode.

--- app/CaffeinatedApp/src/main/java/co/casterlabs/caffeinated/app/AppPreferences.java
+++ app/CaffeinatedApp/src/main/java/co/casterlabs/caffeinated/app/AppPreferences.java
@@ -1,5 +1,7 @@
 package co.casterlabs.caffeinated.app;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import co.casterlabs.caffeinated.util.Crypto;
 import co.casterlabs.kaimen.webview.WebviewRenderer;
 import co.casterlabs.rakurai.json.annotating.JsonClass;
@@ -21,5 +23,15 @@ public class AppPreferences {
             WebviewRenderer.WEBKIT,
             WebviewRenderer.CHROMIUM_EMBEDDED_FRAMEWORK
     };
+
+    public int getConductorPort() {
+        if (CaffeinatedApp.getInstance().isDev()) {
+            // Assign a "random" port when in dev mode. We do this in a method so that when
+            // save() is called we don't accidentally write this port to disk.
+            return this.conductorPort + ThreadLocalRandom.current().nextInt(10) + 1;
+        }
+
+        return this.conductorPort;
+    }
 
 }
