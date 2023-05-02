684851475b1004144eb614b9a751d5570c9019a4
Ender <33337309+e3ndr@users.noreply.github.com>
2023-05-02 08:09:04 -0500

    Caffeinated: Experimental Streamlabs integration :tada:

--- app/CaffeinatedApp/src/main/java/co/casterlabs/caffeinated/app/thirdparty/StreamlabsServicePlugin.java
+++ app/CaffeinatedApp/src/main/java/co/casterlabs/caffeinated/app/thirdparty/StreamlabsServicePlugin.java
@@ -6,21 +6,46 @@ import java.nio.charset.StandardCharsets;
 
 import org.jetbrains.annotations.Nullable;
 
+import co.casterlabs.caffeinated.app.CaffeinatedApp;
 import co.casterlabs.caffeinated.builtin.CaffeinatedDefaultPlugin;
 import co.casterlabs.caffeinated.pluginsdk.CaffeinatedPlugin;
 import co.casterlabs.caffeinated.pluginsdk.widgets.Widget;
 import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails;
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetInstance;
 import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetInstanceMode;
 import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetType;
 import co.casterlabs.caffeinated.util.MimeTypes;
+import co.casterlabs.caffeinated.util.WebUtil;
 import co.casterlabs.commons.functional.tuples.Pair;
+import co.casterlabs.koi.api.listener.KoiEventHandler;
+import co.casterlabs.koi.api.types.events.DonationEvent;
+import co.casterlabs.koi.api.types.events.FollowEvent;
+import co.casterlabs.koi.api.types.events.RaidEvent;
+import co.casterlabs.koi.api.types.events.SubscriptionEvent;
 import co.casterlabs.rakurai.io.IOUtil;
+import co.casterlabs.rakurai.json.Rson;
+import co.casterlabs.rakurai.json.element.JsonBoolean;
+import co.casterlabs.rakurai.json.element.JsonObject;
 import lombok.NonNull;
+import lombok.SneakyThrows;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class StreamlabsServicePlugin extends CaffeinatedPlugin {
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onInit() {
+        CaffeinatedApp
+            .getInstance()
+            .onAppEvent("auth:completion", (JsonObject data) -> {
+                if (data.getString("type").equals("thirdparty") &&
+                    data.getString("platform").equals("streamlabs")) {
+                    this.getLogger().info("Completing OAuth.");
+                    this.completeOAuth();
+                }
+            });
+
         this.getPlugins().registerWidget(this, StreamlabsUI.DETAILS, StreamlabsUI.class);
     }
 
@@ -46,6 +71,78 @@ public class StreamlabsServicePlugin extends CaffeinatedPlugin {
         @Override
         public @NonNull String getWidgetBasePath(WidgetInstanceMode mode) {
             return "/thirdparty/streamlabs/settings";
+        }
+
+        @SneakyThrows
+        private void triggerAlert(String type, String username, String userMessage) {
+            String accessToken = CaffeinatedApp.getInstance().getAuthPreferences().get().getToken("thirdparty", "streamlabs");
+            if (accessToken == null) return;
+
+            WebUtil.sendHttpRequest(
+                new Request.Builder()
+                    .header("Authorization", "Bearer " + accessToken)
+                    .url(
+                        String.format(
+                            "https://streamlabs.com/api/v2.0/alerts?type=%s&message=%s&user_message=%s",
+                            WebUtil.encodeURIComponent(type),
+                            WebUtil.encodeURIComponent(username),
+                            WebUtil.encodeURIComponent(userMessage)
+                        )
+                    )
+                    .post(RequestBody.create(new byte[0]))
+            );
+        }
+
+        @Override
+        public void onNewInstance(@NonNull WidgetInstance instance) {
+            instance.on("hasToken", () -> {
+                String accessToken = CaffeinatedApp.getInstance().getAuthPreferences().get().getToken("thirdparty", "streamlabs");
+
+                try {
+                    instance.emit("hasToken", new JsonBoolean(accessToken != null));
+                } catch (IOException ignored) {}
+            });
+
+            instance.on("setSettings", (e) -> {
+                this.setSettings(e.getAsObject());
+            });
+
+            instance.on("signin", () -> {
+                CaffeinatedApp.getInstance().getAuth().requestOAuthSignin("thirdparty", "streamlabs", false);
+            });
+
+            instance.on("signout", () -> {
+                CaffeinatedApp.getInstance().getAuthPreferences().get().removeToken("thirdparty", "streamlabs");
+                CaffeinatedApp.getInstance().getAuthPreferences().save();
+                try {
+                    instance.emit("hasToken", new JsonBoolean(false));
+                } catch (IOException ignored) {}
+            });
+
+            instance.on("test", () -> {
+                this.triggerAlert("follow", "Casterlabs", "");
+            });
+        }
+
+        @KoiEventHandler
+        public void onFollow(FollowEvent e) {
+            this.triggerAlert("follow", e.getFollower().getDisplayname(), "");
+        }
+
+        @KoiEventHandler
+        public void onSubscription(SubscriptionEvent e) {
+            this.triggerAlert("subscription", "", "");
+        }
+
+        @SuppressWarnings("deprecation")
+        @KoiEventHandler
+        public void onDonation(DonationEvent e) {
+            this.triggerAlert("donation", e.getSender().getDisplayname(), "");
+        }
+
+        @KoiEventHandler
+        public void onRaid(RaidEvent e) {
+            this.triggerAlert("host", e.getHost().getDisplayname(), "");
         }
 
     }
@@ -79,6 +176,32 @@ public class StreamlabsServicePlugin extends CaffeinatedPlugin {
         } catch (Exception e) {
             this.getLogger().debug("An error occurred whilst loading resource %s:\n%s", resource, e);
             return new Pair<>("", "text/plain");
+        }
+    }
+
+    private void completeOAuth() {
+        String code = CaffeinatedApp.getInstance().getAuthPreferences().get().getToken("thirdparty", "streamlabs");
+
+        try {
+            JsonObject response = Rson.DEFAULT.fromJson(
+                WebUtil.sendHttpRequest(
+                    new Request.Builder()
+                        .url("https://api.casterlabs.co/v2/caffeinated/streamlabs/auth/code?code=" + code)
+                ), JsonObject.class
+            );
+
+            String accessToken = response.getString("access_token");
+
+            // Update the auth file.
+            CaffeinatedApp.getInstance().getAuthPreferences().get().addToken("thirdparty", "streamlabs", accessToken);
+            CaffeinatedApp.getInstance().getAuthPreferences().save();
+            this.getLogger().info("OAuth Completed, enabling now.");
+
+            for (Widget w : this.getWidgets()) {
+                w.broadcastToAll("hasToken", new JsonBoolean(true));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
--- ui/src/routes/thirdparty/streamlabs/settings.svelte
+++ ui/src/routes/thirdparty/streamlabs/settings.svelte
@@ -1,1 +1,20 @@
-hello world!
+<script>
+	import SlimButton from '$lib/ui/SlimButton.svelte';
+
+	import { onMount } from 'svelte';
+
+	let hasToken = false;
+
+	onMount(() => {
+		Widget.on('hasToken', (has) => (hasToken = has));
+		Widget.emit('hasToken');
+		console.log(Widget);
+	});
+</script>
+
+{#if hasToken}
+	<SlimButton on:click={() => Widget.emit('signout')}>Signout</SlimButton>
+	<SlimButton on:click={() => Widget.emit('test')}>Test</SlimButton>
+{:else}
+	<SlimButton on:click={() => Widget.emit('signin')}>Signin</SlimButton>
+{/if}
