9adb43afbb19a636e419421fd3fae20aaef2ae9a
Ender <33337309+e3ndr@users.noreply.github.com>
2023-06-18 15:29:59 -0500

    Caffeinated: More platform enums.
    Also cleaned them up.

--- app/Koi/src/main/java/co/casterlabs/koi/api/types/user/UserPlatform.java
+++ app/Koi/src/main/java/co/casterlabs/koi/api/types/user/UserPlatform.java
@@ -1,17 +1,36 @@
 package co.casterlabs.koi.api.types.user;
 
-public enum UserPlatform {
-    CAFFEINE,
-    TWITCH,
-    TROVO,
-    GLIMESH,
-    BRIME,
-    YOUTUBE,
-    DLIVE,
-    THETA,
-    KICK,
-    TIKTOK,
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-    CASTERLABS_SYSTEM;
+@AllArgsConstructor
+public enum UserPlatform {
+    CAFFEINE("Caffeine"),
+    TWITCH("Twitch"),
+    TROVO("Trovo"),
+    YOUTUBE("YouTube"),
+    DLIVE("DLive"),
+    THETA("Theta"),
+    KICK("Kick"),
+    TIKTOK("TikTok"),
+
+    // Coming up.
+    YOUNOW("YouNow"),
+
+    // Graveyard. R.I.P.
+    GLIMESH("Glimesh"),
+    BRIME("Brime"),
+
+    // Other.
+    CASTERLABS_SYSTEM("System"),
+    CUSTOM_INTEGRATION("Custom Integration 🔧"), // For custom events for non-streaming platforms.
+    ;
+
+    private @Getter String str;
+
+    @Override
+    public String toString() {
+        return this.str;
+    }
 
 }
