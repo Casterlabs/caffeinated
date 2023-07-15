8047be79167a35d89e5d920630c09c71c6bd194e
Ender <33337309+e3ndr@users.noreply.github.com>
2023-07-14 23:49:38 -0500

    .

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ if [[ $@ != *"nopackage"* ]]; then
     echo "Completing packaging of application."
     echo ""
 
-    java -jar ProjectBuilder.jar $VM_OPTIONS -os MACOS -arch X86_64 -wi WEBKIT -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar -i icon.icns -id co.casterlabs.caffeinated
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os MACOS -arch X86 -archWord 64 -wi WEBKIT -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar -i icon.icns -id co.casterlabs.caffeinated
     cd dist/macOS-x86_64
     zip -r ../artifacts/macOS-amd64.zip *
     rm -rf ./jre
@@ -31,7 +31,7 @@ if [[ $@ != *"nopackage"* ]]; then
     cd -
     echo ""
 
-    java -jar ProjectBuilder.jar $VM_OPTIONS -os WINDOWS -arch X86_64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -res app/WMC-JsonConsoleWrapper.exe -cp app/Bootstrap/target/Caffeinated.jar -i icon.ico
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os WINDOWS -arch X86 -archWord 64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -res app/WMC-JsonConsoleWrapper.exe -cp app/Bootstrap/target/Caffeinated.jar -i icon.ico
     cd dist/Windows-x86_64
     zip -r ../artifacts/Windows-amd64.zip *
     rm -rf ./jre
@@ -39,7 +39,7 @@ if [[ $@ != *"nopackage"* ]]; then
     cd -
     echo ""
 
-    java -jar ProjectBuilder.jar $VM_OPTIONS -os LINUX -arch X86_64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os LINUX -arch X86 -archWord 64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar
     cd dist/Linux-x86_64
     zip -r ../artifacts/Linux-amd64.zip *
     rm -rf ./jre
