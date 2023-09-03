d8b49798913fc85cc40e3dbd601c574a4eef69dd
Ender <33337309+e3ndr@users.noreply.github.com>
2023-09-03 13:35:28 -0500

    App: Dude.

--- app/CaffeinatedApp/src/main/ui/src/routes/+layout.server.js
+++ app/CaffeinatedApp/src/main/ui/src/routes/+layout.server.js
@@ -1,3 +1,3 @@
 export const trailingSlash = 'never';
 export const prerender = true;
-export const ssr = true; // Changing this breaks the UI during deployments.
+// export const ssr = true; // Changing this breaks the UI during deployments.
