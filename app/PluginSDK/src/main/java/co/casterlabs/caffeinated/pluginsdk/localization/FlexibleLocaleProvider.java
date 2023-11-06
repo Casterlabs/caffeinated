21501fcb756edd014f7e5a64007ef2d25116db5a
Ender <33337309+e3ndr@users.noreply.github.com>
2023-11-05 18:08:14 -0600

    App: Add a regex function provider (#30)

--- app/PluginSDK/src/main/java/co/casterlabs/caffeinated/pluginsdk/localization/FlexibleLocaleProvider.java
+++ app/PluginSDK/src/main/java/co/casterlabs/caffeinated/pluginsdk/localization/FlexibleLocaleProvider.java
@@ -1,13 +1,16 @@
 package co.casterlabs.caffeinated.pluginsdk.localization;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.jetbrains.annotations.Nullable;
 
 import co.casterlabs.commons.functional.Either;
+import co.casterlabs.commons.functional.tuples.Pair;
 import lombok.NonNull;
 
 /**
@@ -28,6 +31,15 @@ public class FlexibleLocaleProvider implements LocaleProvider {
 
     @Override
     public final @Nullable String process(@NonNull String key, @NonNull Map<String, String> knownPlaceholders, @NonNull List<String> knownComponents) {
+        for (Pair<String, LocaleProcessorFunction> entry : this.config.regexes) {
+            String regex = entry.a();
+            LocaleProcessorFunction function = entry.b();
+
+            if (key.matches(regex)) {
+                return function.process(key, knownPlaceholders, knownComponents);
+            }
+        }
+
         Either<String, LocaleProcessorFunction> either = this.config.languageMap.get(key);
         if (either == null) return null;
 
@@ -40,6 +52,7 @@ public class FlexibleLocaleProvider implements LocaleProvider {
 
     public static class Builder {
         private Map<String, Either<String, LocaleProcessorFunction>> languageMap = new HashMap<>();
+        private List<Pair<String, LocaleProcessorFunction>> regexes = new LinkedList<>();
 
         private String prefix;
         private Language language;
@@ -65,9 +78,15 @@ public class FlexibleLocaleProvider implements LocaleProvider {
             return this;
         }
 
+        public final Builder Rfunction(@NonNull String regex, @NonNull LocaleProcessorFunction function) {
+            this.regexes.add(new Pair<>(regex, function));
+            return this;
+        }
+
         public FlexibleLocaleProvider build() {
             FlexibleLocaleProvider instance = new FlexibleLocaleProvider();
             this.languageMap = Collections.unmodifiableMap(this.languageMap);
+            this.regexes = Collections.unmodifiableList(new ArrayList<>(this.regexes));
             instance.config = this;
             return instance;
         }
