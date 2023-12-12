00a17423b37a9a89c188e7578db3ff41a1f382c8
Ender <33337309+e3ndr@users.noreply.github.com>
2023-12-12 14:23:53 -0600

    App: Bring the fr_FR locale up to date.
    Also added stuff for subscriptions.

--- app/CaffeinatedApp/src/main/java/co/casterlabs/caffeinated/app/locale/fr_FR.java
+++ app/CaffeinatedApp/src/main/java/co/casterlabs/caffeinated/app/locale/fr_FR.java
@@ -4,6 +4,8 @@ import java.util.function.Supplier;
 
 import co.casterlabs.commons.localization.LocaleProvider;
 import co.casterlabs.commons.localization.helpers.BuildableLocaleProvider;
+import co.casterlabs.koi.api.types.events.SubscriptionEvent.SubscriptionLevel;
+import co.casterlabs.koi.api.types.events.SubscriptionEvent.SubscriptionType;
 
 public class fr_FR implements Supplier<LocaleProvider> {
 
@@ -24,80 +26,77 @@ public class fr_FR implements Supplier<LocaleProvider> {
             })
 
             // TODO
-//            .function("docks.chat.viewer.event_format.SUBSCRIPTION", (key, externalLookup, knownPlaceholders, knownComponents) -> {
-//                // This gets ugly REAL quick.
-//                double monthsPurchased = Double.parseDouble(knownPlaceholders.get("months_purchased"));
-//                double monthsStreak = Double.parseDouble(knownPlaceholders.get("months_streak"));
-//                SubscriptionLevel level = SubscriptionLevel.valueOf(knownPlaceholders.get("level"));
-//                SubscriptionType type = SubscriptionType.valueOf(knownPlaceholders.get("type"));
-//
-//                switch (type) {
-//                    case SUB:
-//                    case RESUB: {
-//                        String format = "%name% just subscribed";
-//                        switch (level) {
-//                            case TIER_1:
-//                                format += " at Tier 1";
-//                            case TIER_2:
-//                                format += " at Tier 2";
-//                            case TIER_3:
-//                                format += " at Tier 3";
-//                            case TIER_4:
-//                                format += " at Tier 4";
-//                            case TIER_5:
-//                                format += " at Tier 5";
-//                            case TWITCH_PRIME:
-//                                format += " with Twitch Prime";
-//                            case UNKNOWN:
-//                                break; // No touch.
-//                        }
-//                        if (monthsPurchased > 1) {
-//                            format += " for %months_purchased% months";
-//                        }
-//                        if (monthsStreak > 1) {
-//                            format += "! They have been subscribed for %months_streak% months";
-//                        }
-//                        format += "!";
-//                        return format;
-//                    }
-//
-//                    case ANONSUBGIFT: // %name% will be "Anonymous".
-//                    case ANONRESUBGIFT: // %name% will be "Anonymous".
-//                    case SUBGIFT:
-//                    case RESUBGIFT: {
-//                        String format = "%name% just gifted %recipient%";
-//                        if (monthsPurchased > 1) {
-//                            format += " a %months_purchased% month";
-//                        } else {
-//                            format += " a";
-//                        }
-//                        switch (level) {
-//                            case TIER_1:
-//                                format += " Tier 1";
-//                            case TIER_2:
-//                                format += " Tier 2";
-//                            case TIER_3:
-//                                format += " Tier 3";
-//                            case TIER_4:
-//                                format += " Tier 4";
-//                            case TIER_5:
-//                                format += " Tier 5";
-//                            case TWITCH_PRIME:
-//                                format += " Twitch Prime";
-//                            case UNKNOWN:
-//                                break; // No touch.
-//                        }
-//                        format += " subscription";
-//                        if (monthsStreak > 1) {
-//                            format += "! They have been subscribed for %months_streak% months";
-//                        }
-//                        format += "!";
-//                        return format;
-//                    }
-//                }
-//
-//                return null;
-//            })
+            .function("docks.chat.viewer.event_format.SUBSCRIPTION", (key, externalLookup, knownPlaceholders, knownComponents) -> {
+                // This gets ugly REAL quick.
+                double monthsPurchased = Double.parseDouble(knownPlaceholders.get("months_purchased"));
+                double monthsStreak = Double.parseDouble(knownPlaceholders.get("months_streak"));
+                SubscriptionLevel level = SubscriptionLevel.valueOf(knownPlaceholders.get("level"));
+                SubscriptionType type = SubscriptionType.valueOf(knownPlaceholders.get("type"));
+
+                switch (type) {
+                    case SUB:
+                    case RESUB: {
+                        String format = "%name% vient de s'abonner";
+                        switch (level) {
+                            case TIER_1:
+                                format += " au Tier 1";
+                            case TIER_2:
+                                format += " au Tier 2";
+                            case TIER_3:
+                                format += " au Tier 3";
+                            case TIER_4:
+                                format += " au Tier 4";
+                            case TIER_5:
+                                format += " at Tier 5";
+                            case TWITCH_PRIME:
+                                format += " avec Twitch Prime";
+                            case UNKNOWN:
+                                break; // No touch.
+                        }
+                        if (monthsPurchased > 1) {
+                            format += " pour %months_purchased% mois";
+                        }
+                        if (monthsStreak > 1) {
+                            format += " ! Ils sont abonnés depuis %months_streak% mois";
+                        }
+                        format += " !";
+                        return format;
+                    }
+
+                    case ANONSUBGIFT: // %name% will be "Anonymous".
+                    case ANONRESUBGIFT: // %name% will be "Anonymous".
+                    case SUBGIFT:
+                    case RESUBGIFT: {
+                        String format = "%name% vient d'offrir %recipient% un abonnement";
+                        switch (level) {
+                            case TIER_1:
+                                format += " Tier 1";
+                            case TIER_2:
+                                format += " Tier 2";
+                            case TIER_3:
+                                format += " Tier 3";
+                            case TIER_4:
+                                format += " Tier 4";
+                            case TIER_5:
+                                format += " Tier 5";
+                            case TWITCH_PRIME:
+                                format += " Twitch Prime";
+                            case UNKNOWN:
+                                break; // No touch.
+                        }
+                        if (monthsPurchased > 1) {
+                            format += " de %months_purchased% mois";
+                        }
+                        if (monthsStreak > 1) {
+                            format += " ! Ils sont abonnés depuis %months_streak% mois";
+                        }
+                        format += " !";
+                        return format;
+                    }
+                }
+
+                return null;
+            })
             .build();
     }
 
