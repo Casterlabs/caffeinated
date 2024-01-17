87d170b44140bb2ba215daffc90a86173ba060a9
Ender <33337309+e3ndr@users.noreply.github.com>
2024-01-16 23:02:18 -0600

    App: Rename Bootstrap->App.

--- app/dev.sh
+++ app/dev.sh
@@ -3,4 +3,4 @@
 # Copy the widget loader to the appropriate places...
 ./mvnw generate-sources
 # Then run the dev servers.
-npx concurrently "cd Bootstrap/src/main/ui && npm install && npm run dev" "cd BuiltInPlugins/src/main/widgets && npm install && npm run dev"
+npx concurrently "cd App/src/main/ui && npm install && npm run dev" "cd BuiltInPlugins/src/main/widgets && npm install && npm run dev"
--- build.sh
+++ build.sh
@@ -18,14 +18,14 @@ KAIMEN_VERSION="3cb3816"
 VM_OPTIONS="-vm Xms1M -vm XX:+UseCompressedOops -vm XX:+UseSerialGC -vm XX:MaxHeapFreeRatio=1 -vm XX:MinHeapFreeRatio=1"
 
 if [[ $@ != *"nopackage"* ]]; then
-    cp app/Bootstrap/target/classes/commit.txt dist
+    cp app/App/target/classes/commit.txt dist
     mkdir dist/artifacts
 
     echo ""
     echo "Completing packaging of application."
     echo ""
 
-    java -jar ProjectBuilder.jar $VM_OPTIONS -os MACOS -arch X86 -archWord 64 -wi WEBKIT -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar -i icon.icns -id co.casterlabs.caffeinated
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os MACOS -arch X86 -archWord 64 -wi WEBKIT -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/App/target/Caffeinated.jar -i icon.icns -id co.casterlabs.caffeinated
     cd dist/macOS-x86_64
     zip -r ../artifacts/macOS-amd64.zip *
     rm -rf ./jre
@@ -33,7 +33,7 @@ if [[ $@ != *"nopackage"* ]]; then
     cd -
     echo ""
 
-    java -jar ProjectBuilder.jar $VM_OPTIONS -os WINDOWS_NT -arch X86 -archWord 64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -res app/WMC-JsonConsoleWrapper.exe -cp app/Bootstrap/target/Caffeinated.jar -i icon.ico
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os WINDOWS_NT -arch X86 -archWord 64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -res app/WMC-JsonConsoleWrapper.exe -cp app/App/target/Caffeinated.jar -i icon.ico
     cd dist/Windows-x86_64
     zip -r ../artifacts/Windows-amd64.zip *
     rm -rf ./jre
@@ -41,7 +41,7 @@ if [[ $@ != *"nopackage"* ]]; then
     cd -
     echo ""
 
-    java -jar ProjectBuilder.jar $VM_OPTIONS -os LINUX -arch X86 -archWord 64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/Bootstrap/target/Caffeinated.jar
+    java -jar ProjectBuilder.jar $VM_OPTIONS -os LINUX -arch X86 -archWord 64 -wi CHROMIUM_EMBEDDED_FRAMEWORK -v 1.2 -n "Casterlabs-Caffeinated" -jv JAVA11 -kv $KAIMEN_VERSION -cp app/App/target/Caffeinated.jar
     cd dist/Linux-x86_64
     zip -r ../artifacts/Linux-amd64.zip *
     rm -rf ./jre
