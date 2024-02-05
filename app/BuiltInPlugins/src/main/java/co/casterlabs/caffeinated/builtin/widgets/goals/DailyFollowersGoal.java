e56fc721bff0ee4d9ec3c334af8c0a5a6a37bdbe
Ender <33337309+e3ndr@users.noreply.github.com>
2024-02-04 23:48:18 -0600

    App: Daily followers & subscribers goals!

---
+++ app/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/widgets/goals/DailyFollowersGoal.java
@@ -0,0 +1,76 @@
+package co.casterlabs.caffeinated.builtin.widgets.goals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails;
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails.WidgetDetailsCategory;
+import co.casterlabs.koi.api.KoiIntegrationFeatures;
+import co.casterlabs.koi.api.listener.KoiEventHandler;
+import co.casterlabs.koi.api.types.events.FollowEvent;
+import co.casterlabs.koi.api.types.events.StreamStatusEvent;
+
+public class DailyFollowersGoal extends GenericGoal {
+    public static final WidgetDetails DETAILS = new WidgetDetails()
+        .withNamespace("co.casterlabs.daily_followers_goal")
+        .withIcon("users")
+        .withCategory(WidgetDetailsCategory.GOALS)
+        .withFriendlyName("Daily Followers Goal")
+        .withShowDemo(true, DEMO_ASPECT_RATIO)
+        .withRequiredFeatures(KoiIntegrationFeatures.FOLLOWER_ALERT, KoiIntegrationFeatures.STREAM_STATUS);
+
+    private Map<String, Integer> offsets = new HashMap<>();
+
+    @Override
+    public void onInit() {
+        this.addKoiListener(this);
+        this.recalculate();
+    }
+
+    @Override
+    protected void onSettingsUpdate() {
+        super.onSettingsUpdate();
+        this.recalculate();
+    }
+
+    @KoiEventHandler
+    public void onFollow(FollowEvent e) {
+        if (this.offsets.containsKey(e.getStreamer().getUPID())) {
+            int newOffset = this.offsets.get(e.getStreamer().getUPID()) + 1;
+            this.offsets.put(e.getStreamer().getUPID(), newOffset);
+        }
+        this.recalculate();
+    }
+
+    @KoiEventHandler
+    public void onStreamStatus(StreamStatusEvent e) {
+        if (e.isLive()) {
+            this.offsets.putIfAbsent(e.getStreamer().getUPID(), 0);
+        } else {
+            this.offsets.remove(e.getStreamer().getUPID()); // Clear the offset value. No longer live.
+        }
+        this.recalculate();
+    }
+
+    private void recalculate() {
+        long count = 0;
+        for (Integer c : this.offsets.values()) {
+            count += c;
+        }
+        this.update(count);
+    }
+
+    @Override
+    protected boolean enablePlatformOption() {
+        return true;
+    }
+
+    @Override
+    protected KoiIntegrationFeatures[] requiredPlatformFeatures() {
+        return new KoiIntegrationFeatures[] {
+                KoiIntegrationFeatures.FOLLOWER_ALERT,
+                KoiIntegrationFeatures.STREAM_STATUS
+        };
+    }
+
+}
---
+++ app/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/widgets/goals/DailySubscribersGoal.java
@@ -0,0 +1,76 @@
+package co.casterlabs.caffeinated.builtin.widgets.goals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails;
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails.WidgetDetailsCategory;
+import co.casterlabs.koi.api.KoiIntegrationFeatures;
+import co.casterlabs.koi.api.listener.KoiEventHandler;
+import co.casterlabs.koi.api.types.events.StreamStatusEvent;
+import co.casterlabs.koi.api.types.events.SubscriptionEvent;
+
+public class DailySubscribersGoal extends GenericGoal {
+    public static final WidgetDetails DETAILS = new WidgetDetails()
+        .withNamespace("co.casterlabs.daily_subscribers_goal")
+        .withIcon("user-plus")
+        .withCategory(WidgetDetailsCategory.GOALS)
+        .withFriendlyName("Daily Subscribers Goal")
+        .withShowDemo(true, DEMO_ASPECT_RATIO)
+        .withRequiredFeatures(KoiIntegrationFeatures.SUBSCRIPTION_ALERT, KoiIntegrationFeatures.STREAM_STATUS);
+
+    private Map<String, Integer> offsets = new HashMap<>();
+
+    @Override
+    public void onInit() {
+        this.addKoiListener(this);
+        this.recalculate();
+    }
+
+    @Override
+    protected void onSettingsUpdate() {
+        super.onSettingsUpdate();
+        this.recalculate();
+    }
+
+    @KoiEventHandler
+    public void onSubscribe(SubscriptionEvent e) {
+        if (this.offsets.containsKey(e.getStreamer().getUPID())) {
+            int newOffset = this.offsets.get(e.getStreamer().getUPID()) + 1;
+            this.offsets.put(e.getStreamer().getUPID(), newOffset);
+        }
+        this.recalculate();
+    }
+
+    @KoiEventHandler
+    public void onStreamStatus(StreamStatusEvent e) {
+        if (e.isLive()) {
+            this.offsets.putIfAbsent(e.getStreamer().getUPID(), 0);
+        } else {
+            this.offsets.remove(e.getStreamer().getUPID()); // Clear the offset value. No longer live.
+        }
+        this.recalculate();
+    }
+
+    private void recalculate() {
+        long count = 0;
+        for (Integer c : this.offsets.values()) {
+            count += c;
+        }
+        this.update(count);
+    }
+
+    @Override
+    protected boolean enablePlatformOption() {
+        return true;
+    }
+
+    @Override
+    protected KoiIntegrationFeatures[] requiredPlatformFeatures() {
+        return new KoiIntegrationFeatures[] {
+                KoiIntegrationFeatures.SUBSCRIPTION_ALERT,
+                KoiIntegrationFeatures.STREAM_STATUS
+        };
+    }
+
+}
