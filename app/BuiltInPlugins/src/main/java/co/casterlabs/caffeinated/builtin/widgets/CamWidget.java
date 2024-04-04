7ba0e6845b17e7b17f85dfd0cc53e2a73c61aade
Ender <33337309+e3ndr@users.noreply.github.com>
2024-04-04 13:29:01 -0500

    App: Use the new studio-based cam UI.
    This is pog.

--- app/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/widgets/CamWidget.java
+++ app/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/widgets/CamWidget.java
@@ -62,7 +62,7 @@ public class CamWidget extends Widget implements KinokoV1Listener {
                         .withIconTitle("Copy Camera Link")
                         .withOnClick(() -> {
                             Caffeinated.getInstance().copyText(
-                                String.format("https://casterlabs.co/tools/cam?id=%s", this.settings().getString("cam.id")),
+                                String.format("https://studio.casterlabs.co/tools/cam?id=%s", this.settings().getString("cam.id")),
                                 null
                             );
                         })
