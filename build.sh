4a6ddde1fae78c5f907cd0a695d02ab4e63cef77
e3ndr <33337309+e3ndr@users.noreply.github.com>
2024-10-01 20:34:48 -0500

    App: Fix a deadlock in Kaimen.

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ fi
 rm -rf dist/*
 mkdir -p -p dist
 
-KAIMEN_VERSION="99e3999"
+KAIMEN_VERSION="4689d69"
 VM_OPTIONS="-vm Xms1M -vm XX:+UseCompressedOops -vm XX:+UseSerialGC -vm XX:MaxHeapFreeRatio=1 -vm XX:MinHeapFreeRatio=1"
 
 if [[ $@ != *"nopackage"* ]]; then
