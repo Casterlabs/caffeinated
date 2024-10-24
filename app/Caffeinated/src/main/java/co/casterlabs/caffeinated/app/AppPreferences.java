3021f741acd10b342fb460d9c70ce72f3b42a5fd
e3ndr <33337309+e3ndr@users.noreply.github.com>
2024-10-24 06:58:53 -0500

    App: Adjust conductor offets.

--- app/Caffeinated/src/main/java/co/casterlabs/caffeinated/app/AppPreferences.java
+++ app/Caffeinated/src/main/java/co/casterlabs/caffeinated/app/AppPreferences.java
@@ -2,7 +2,6 @@ package co.casterlabs.caffeinated.app;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
 
 import co.casterlabs.caffeinated.util.Crypto;
 import co.casterlabs.rakurai.json.annotating.JsonClass;
@@ -13,8 +12,6 @@ import lombok.Data;
 @Data
 @JsonClass(exposeAll = true)
 public class AppPreferences {
-    private static final int RANDOM_OFFSET = ThreadLocalRandom.current().nextInt(10) + 1;
-
     private int conductorPort = 8092; // Caffeinated <1.2 was 8091.
     private String conductorKey = new String(Crypto.generateSecureRandomKey());
     private String developerApiKey = new String(Crypto.generateSecureRandomKey());
@@ -27,7 +24,7 @@ public class AppPreferences {
         if (CaffeinatedApp.getInstance().isDev()) {
             // Assign a "random" port when in dev mode. We do this in a method so that when
             // save() is called we don't accidentally write this port to disk.
-            return this.conductorPort + RANDOM_OFFSET;
+            return this.conductorPort + 1;
         }
 
         return this.conductorPort;
