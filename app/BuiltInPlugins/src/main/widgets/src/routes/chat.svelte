1d6a0df1ccda60d29f8ac54c2c47e60be7d38994
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-03-28 07:57:58 -0500

    App: Fix message deletions :^)

--- app/BuiltInPlugins/src/main/widgets/src/routes/chat.svelte
+++ app/BuiltInPlugins/src/main/widgets/src/routes/chat.svelte
@@ -51,11 +51,16 @@
 				if (event.user_upid) {
 					// Clear by user.
 					for (const [key, { component: chatMessage }] of Object.entries(chatElements)) {
-						const koiEvent = chatMessage.koiEvent;
+						try {
+ 							const koiEvent = chatMessage.event;
+ 							const koiSender = koiEvent.sender || koiEvent.follower || koiEvent.subscriber //
+ 							|| koiEvent.host || koiEvent.viewer || koiEvent.gift_recipient || koiEvent.liker;
 
-						if (koiEvent.sender && koiEvent.sender.UPID == event.user_upid) {
-							chatMessage.remove();
-							delete chatElements[key];
+							if (koiSender && koiSender.UPID == event.user_upid) {
+ 								chatMessage.isDeleted = true;
+ 							}
+ 						} catch (e) {
+ 							console.error('Error while clearing chat:', e);
 						}
 					}
 					return;
