f5f02eb1fa4db052bee8f40aa8414954f30b4039
e3ndr <33337309+e3ndr@users.noreply.github.com>
2026-01-09 08:19:09 -0600

    App: Fix checkboxes...?

--- app/ui/src/lib/layout/widget-settings/_BooleanInput.svelte
+++ app/ui/src/lib/layout/widget-settings/_BooleanInput.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { Input } from '@casterlabs/ui';
 
-	import { onMount } from 'svelte';
-
 	interface Props {
 		widget: Awaited<typeof AppPlugins.widgets>[0];
 		settingsKey: string;
@@ -10,22 +8,14 @@
 	}
 
 	let { widget, settingsKey, settingsItem }: Props = $props();
-	let container: HTMLDivElement;
 
 	let checked = $state(widget.settings[settingsKey] as boolean);
-
-	onMount(() => {
-		// I have no fucking idea.
-		container.querySelector('input')!.checked = checked;
-	});
 </script>
 
-<div class="contents" bind:this={container}>
-	<Input
-		type="checkbox"
-		bind:checked
-		onchange={() => {
-			AppPlugins.editWidgetSettingsItem(widget!.id, settingsKey, checked);
-		}}
-	/>
-</div>
+<Input
+	type="checkbox"
+	bind:checked
+	onchange={() => {
+		AppPlugins.editWidgetSettingsItem(widget!.id, settingsKey, checked);
+	}}
+/>
