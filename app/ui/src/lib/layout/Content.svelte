f804c1b200c02f95fa8f0fabce21e24fe518925f
e3ndr <33337309+e3ndr@users.noreply.github.com>
2026-08-14 14:58:52 -0500

    App: Remove padding on docks.

--- app/ui/src/lib/layout/Content.svelte
+++ app/ui/src/lib/layout/Content.svelte
@@ -1,3 +1,9 @@
-<main id="main" class="flex-1 h-full overflow-auto px-6 py-4">
+<script lang="ts">
+	// @ts-ignore
+	const isDock = typeof window.Widget !== 'undefined';
+</script>
+
+<main id="main" class="flex-1 h-full overflow-auto" class:px-6={!isDock} class:py-4={!isDock}>
+	<!-- svelte-ignore slot_element_deprecated -->
 	<slot />
 </main>
